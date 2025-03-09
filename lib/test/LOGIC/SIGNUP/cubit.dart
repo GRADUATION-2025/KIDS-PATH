aef8c8edc2fc59ba7b2902c02e03f98e96b27737
Omar Hany Mostafa <amorahany36002@gmail.com>
2025-03-10 01:43:01 +0200

    Initial commit

---
+++ lib/test/LOGIC/SIGNUP/cubit.dart
@@ -0,0 +1,23 @@
+
+import 'package:firebase_auth/firebase_auth.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:kidspath/test/LOGIC/SIGNUP/state.dart';
+
+
+
+class SignupCubit extends Cubit<SignUpStates>{
+  FirebaseAuth firebaseAuth;
+  SignupCubit(this.firebaseAuth):super(SignUpInitialState());
+
+
+  Future signup(String email,String password)async{
+    emit(SignUpLoadingState());
+    try{
+      await firebaseAuth.createUserWithEmailAndPassword(email: email, password: password);
+      emit(SignupSuccessState());
+    }
+    catch(e){
+      emit(SignUpErrorState(e.toString()));
+    }
+  }
+}
