9dc92502d47f22bc0b84bf03334d40ad32f2ffcc
youssefkaram <kukysemsem@gmail.com>
2025-03-14 22:21:27 +0200

    change

--- lib/UI/WELCOME SCREENS/REGISTER_SCREEN.dart
+++ lib/UI/WELCOME SCREENS/REGISTER_SCREEN.dart
@@ -1,168 +1,3 @@
-// import 'package:firebase_auth/firebase_auth.dart';
-// import 'package:flutter/material.dart';
-// import 'package:flutter_bloc/flutter_bloc.dart';
-// import 'package:flutter_screenutil/flutter_screenutil.dart';
-// import '../../LOGIC/SIGNUP/cubit.dart';
-// import '../../LOGIC/SIGNUP/state.dart';
-// import '../../home.dart';
-// import 'LOGIN_SCREEN.dart';
-//
-// class RegisterScreen extends StatefulWidget {
-//   const RegisterScreen({super.key});
-//
-//   @override
-//   State<RegisterScreen> createState() => _RegisterScreenState();
-// }
-//
-// class _RegisterScreenState extends State<RegisterScreen> {
-//   TextEditingController emailController = TextEditingController();
-//   TextEditingController passwordController = TextEditingController();
-//   final formKey = GlobalKey<FormState>();
-//
-//   @override
-//   Widget build(BuildContext context) {
-//     return BlocProvider(
-//       create: (context) => SignupCubit(FirebaseAuth.instance),
-//       child: BlocConsumer<SignupCubit, SignUpStates>(
-//         listener: (context, state) {
-//           if (state is SignupSuccessState) {
-//             ScaffoldMessenger.of(context).showSnackBar(
-//               const SnackBar(content: Text("User Created Successfully")),
-//             );
-//             Navigator.pushReplacement(
-//               context,
-//               MaterialPageRoute(builder: (context) => Home()),
-//             );
-//           } else if (state is SignUpErrorState) {
-//             ScaffoldMessenger.of(context).showSnackBar(
-//               SnackBar(content: Text(state.errorMessage)),
-//             );
-//           }
-//         },
-//         builder: (context, state) {
-//           return Scaffold(
-//             body: Container(
-//               decoration: const BoxDecoration(
-//                 gradient: LinearGradient(
-//                   begin: Alignment.topCenter,
-//                   end: Alignment.bottomCenter,
-//                   colors: [Color(0xFF07C8F9), Color(0xFF0D41E1)],
-//                 ),
-//               ),
-//               child: Padding(
-//                 padding:  EdgeInsets.all(20.r),
-//                 child: Column(
-//                   mainAxisAlignment: MainAxisAlignment.center,
-//                   children: [
-//                     SizedBox(height: 12.h,),
-//                      Text(
-//                       "Kids Path",
-//                       style: TextStyle(
-//                         fontSize: 60.sp,
-//                         fontWeight: FontWeight.bold,
-//                         color: Colors.white,
-//                       ),
-//                     ),
-//                      SizedBox(height: 30.h),
-//                     Form(
-//                       key: formKey,
-//                       child: Column(
-//                         children: [
-//                           TextFormField(
-//                             controller: emailController,
-//                             decoration: InputDecoration(
-//                               filled: true,
-//                               fillColor: Colors.white,
-//                               labelText: "Email",
-//                               labelStyle: const TextStyle(color: Colors.black),
-//                               border: OutlineInputBorder(
-//                                 borderRadius: BorderRadius.circular(12.r),
-//                                 borderSide: BorderSide.none,
-//                               ),
-//                             ),
-//                           ),
-//                            SizedBox(height: 15.h),
-//                           TextFormField(
-//                             controller: passwordController,
-//                             decoration: InputDecoration(
-//                               filled: true,
-//                               fillColor: Colors.white,
-//                               labelText: "Password",
-//                               labelStyle: const TextStyle(color: Colors.black),
-//                               border: OutlineInputBorder(
-//                                 borderRadius: BorderRadius.circular(12.r),
-//                                 borderSide: BorderSide.none,
-//                               ),
-//                             ),
-//                             obscureText: true,
-//                           ),
-//                         ],
-//                       ),
-//                     ),
-//                      SizedBox(height: 20.h),
-//                     InkWell(
-//                       onTap: () {
-//                         if (formKey.currentState?.validate() ?? false) {
-//                           final email = emailController.text;
-//                           final password = passwordController.text;
-//                           context.read<SignupCubit>().signup(email, password);
-//                         }
-//                       },
-//                       child: Container(
-//                         width: double.infinity,
-//                         height: 50.h,
-//                         decoration: BoxDecoration(
-//                           color: Colors.white,
-//                           borderRadius: BorderRadius.circular(12.r),
-//                         ),
-//                         child: Center(
-//                           child: state is SignUpLoadingState
-//                               ?  CircularProgressIndicator(color: Colors.blueAccent)
-//                               :  Text(
-//                             'Sign In',
-//                             style: TextStyle(
-//                               fontSize: 16.sp,
-//                               color: Colors.black,
-//                               fontWeight: FontWeight.bold,
-//                             ),
-//                           ),
-//                         ),
-//                       ),
-//                     ),
-//                      SizedBox(height: 15.h),
-//                     Row(
-//                       mainAxisAlignment: MainAxisAlignment.center,
-//                       children: [
-//                         const Text("Have an Account?",style: TextStyle(color: Colors.white),),
-//                         TextButton(
-//                           onPressed: () {
-//                             Navigator.pushReplacement(
-//                               context,
-//                               MaterialPageRoute(
-//                                 builder: (context) => LoginScreen(),
-//                               ),
-//                             );
-//                           },
-//                           child: const Text(
-//                             'Sign In',
-//                             style: TextStyle(
-//                               fontWeight: FontWeight.bold,
-//                               color: Colors.white,
-//                             ),
-//                           ),
-//                         ),
-//                       ],
-//                     ),
-//                   ],
-//                 ),
-//               ),
-//             ),
-//           );
-//         },
-//       ),
-//     );
-//   }
-// }
 import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
@@ -240,7 +75,7 @@ class _RegisterScreenState extends State<RegisterScreen> {
                               filled: true,
                               fillColor: Colors.white,
                               labelText: "Email",
-                              labelStyle: const TextStyle(color: Colors.grey),
+                              labelStyle: const TextStyle(color: Colors.black),
                               prefixIcon: Icon(Icons.email, color: Color(0xFF08203E)), // Email Icon
                               border: OutlineInputBorder(
                                 borderRadius: BorderRadius.circular(12.r),
@@ -256,7 +91,7 @@ class _RegisterScreenState extends State<RegisterScreen> {
                               filled: true,
                               fillColor: Colors.white,
                               labelText: "Password",
-                              labelStyle: const TextStyle(color: Colors.grey),
+                              labelStyle: const TextStyle(color: Colors.black),
                               prefixIcon: Icon(Icons.lock, color: Color(0xFF08203E)), // Password Icon
                               border: OutlineInputBorder(
                                 borderRadius: BorderRadius.circular(12.r),
