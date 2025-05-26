ff2c2eed2772ecf2b1d8e7ecd0c0a389132a4f03
Omar Hany Mostafa <amorahany36002@gmail.com>
2025-05-26 11:13:54 +0300

    final edit 5 & privacy policy page

--- lib/UI/Create_Profile_screen/NURSERY/NURSERY_PAGE.dart
+++ lib/UI/Create_Profile_screen/NURSERY/NURSERY_PAGE.dart
@@ -133,7 +133,7 @@ class NurseryAccountScreen extends StatelessWidget {
                       MaterialPageRoute(builder: (context) => BottombarNurseryScreen(initialIndex: 2,)),
                           (route) => false,
                     ),),
-                    accountOption(Icons.privacy_tip, "Privacy Settings",
+                    accountOption(Icons.privacy_tip, "Privacy and Poilcy",
                     onTap: ()=> Navigator.push(context,
                         MaterialPageRoute(builder: (context) => PrivacyPolicyScreen(),))),
                     accountOption(
